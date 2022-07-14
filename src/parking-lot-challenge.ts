2dbb11a0babb9ab2462b4f5988f83f36da292760
Eddie Naff <eddie@eddienaff.dev>
2022-07-14 14:08:16 -0700

    Implement methods

--- src/parking-lot-challenge.ts
+++ src/parking-lot-challenge.ts
@@ -1,5 +1,7 @@
-class ParkingLot {
-  slots: string[] = [];
+type SlotType = string | null;
+
+export class ParkingLot {
+  slots: SlotType[] = [];
   constructor(parkingSize: number) {
     this.slots = new Array(parkingSize).fill(null);
   }
@@ -7,32 +9,89 @@ class ParkingLot {
   /**
    * A method that shows us how many available slots we currently have
    */
-  getAvailable() {}
+  getAvailable() {
+    const availableSlots = this.slots.filter((slot) => slot === null).length;
+    console.log(`Available parking slots: ${availableSlots}`);
+    return availableSlots;
+  }
 
   /**
    * A helper method that we use to check the parking lot size.
    */
-  getSize() {}
+  getSize() {
+    console.log(`Parking size is: ${this.slots.length}`);
+    return this.slots.length;
+  }
 
   /**
-   * Helper method that just returns the array we use to store the parking slots.
+   * Returns the array we use to store the parking slots.
    */
-  getSlots() {}
+  getSlots() {
+    console.log(`Parking slots: ${this.slots}`);
+    return this.slots;
+  }
 
-  /** Tells us if the parking lot is full,
-   * that is that there are no more available slots. */
-  isAvailable() {}
+  /**
+   * Tells us if the parking lot is full,
+   * that is that there are no more available slots.
+   */
+  isAvailable() {
+    console.log(`No parking spots are available`);
+  }
+
+  /**
+   * Tells us if the parking lot does not have available slots.
+   * @returns boolean;
+   */
+  isFull() {
+    return this.getAvailable() === 0;
+  }
 
   /**
    * This method iterates over the slots array, checks if there are free
    * spots (that is, slots that are still equal to null), and adds in
    * the car in those empty spots.
    */
-  park() {}
+  park(carId: string) {
+    console.log(`Parking car: ${carId}`);
+    // If we can't find an empty space return false.
+    if (this.slots.every((slot) => slot !== carId)) {
+      return false;
+    }
+
+    // Iterate over our slots to match it to the prvovided carId
+    // Once we find a match reassign the value at that array position to carId
+    for (let slotIndex = 0; slotIndex < this.slots.length; slotIndex++) {
+      const slot = this.slots[slotIndex];
+
+      if (slot === null) {
+        this.slots[slotIndex] = carId;
+        return true;
+      }
+    }
+  }
 
   /**
    * This is how we remove cars from the parking lot.
    * This method also iterates over the slots array
    */
-  remove() {}
+  remove(carId: string) {
+    console.log(`Leaving car: ${carId}`);
+    // If we can't find the carId return false,
+    // meaning we haven't remvoed it.
+    if (this.slots.every((slot) => slot !== carId)) {
+      return false;
+    }
+
+    // Iterate over our slots to match it to carId
+    // Once we find a match reassign the value at that array position to null
+    for (let slotIndex = 0; slotIndex < this.slots.length; slotIndex++) {
+      const slot = this.slots[slotIndex];
+
+      if (slot === carId) {
+        this.slots[slotIndex] = null;
+        return true;
+      }
+    }
+  }
 }
